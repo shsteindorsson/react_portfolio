66cf75940509635f36ca2a391e49bbcd6d9347f8
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-17 15:35:32 +0200

    Add header, footer and project cards

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,18 @@
 import React from "react"
+import { UndrawDesignerLife } from 'react-undraw-illustrations'
 
 const About = () => {
   return (
     <div className="ui container"> 
-      <h1 className="ui header">About Me</h1>
-      <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+      <div class="ui stackable two column grid">
+        <div class="column">
+          <UndrawDesignerLife />
+        </div>
+        <div class="column">
+          <h1 className="ui header">About Me</h1>
+          <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+        </div>
+      </div>
     </div>)
 }
 
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,49 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
+import ProjectCard from "./ProjectCard"
 
-const Projects = () => {
-  return (
-    <div className="ui container">
-      <h1 className="ui header">My Projects</h1>
-      <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring</p>
-    </div>
-  )
+class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    };
+  }
 
+  componentDidMount() {
+    axios.get('./src/data/projects.json')
+      .then(response => {
+        this.setState({
+          projects: response.data
+        })
+      })
+    }
+
+  render() {
+    const projects = this.state.projects
+    let projectsList
+
+    if (projects.length > 0) {
+      projectsList = projects.map(project => {
+        return (
+          <div key={project.id}>
+            <ProjectCard project={project} />
+          </div>
+        )
+      })
+    }
+
+
+    return (
+
+      <div className="ui main container">
+        <h1 className="ui header">My Projects</h1>
+        <div className="ui stackable four column grid">
+          {projectsList}
+        </div>
+      </div>
+    )
+  }
 };
 
 export default Projects
