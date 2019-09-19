13edfeba28eb5a5b42f1f3673eea38328bed8b3d
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-19 12:41:55 +0200

    Add images to repo

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,10 +3,13 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Hello = () => {
   return (
-    <div className="ui main container" id="hello-site">
-      <h1>
-        Greetings, World
-      </h1>
+    <div id="hello-site">
+      <div id="greeting-div">
+        <h1>Greetings, I'm Sverrir</h1>
+        <p>I prefer to work under the hood and let other people deal with the exterior.</p>  
+      </div>
+      <br></br>
+      <img src="https://github.com/shsteindorsson/shsteindorsson.github.io/blob/master/images/8bit_shs.png?raw=true"></img>
     </div>
   )
 }
